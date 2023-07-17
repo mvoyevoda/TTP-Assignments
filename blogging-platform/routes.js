65488da402fd0599a5546b9f27aae1292737eedf
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-07-17 17:14:35 -0400

    setup CRUD for Posts/Comments

--- blogging-platform/routes.js
+++ blogging-platform/routes.js
@@ -46,6 +46,70 @@ router.get('/posts/:id', async (req, res) => {
   }
 });
 
+router.post('/posts', async (req, res) => {
+  try {
+    const { author, content } = req.body; // Get post data from the request body
+
+    // Create the post
+    const post = await Post.create({
+      author: author,
+      content: content
+    });
+
+    res.status(201).json(post); // Return the created post in the response
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.put('/posts/:id', async (req, res) => {
+  try {
+    const postId = req.params.id; // Get the post ID from the request parameters
+    const { author, content } = req.body; // Get updated post data from the request body
+
+    // Find the post by its ID
+    const post = await Post.findByPk(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    // Update the post with new data
+    post.author = author;
+    post.content = content;
+    await post.save();
+
+    res.json(post); // Return the updated post in the response
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+router.delete('/posts/:id', async (req, res) => {
+  try {
+    const postId = req.params.id; // Get the post ID from the request parameters
+
+    // Find the post by its ID
+    const post = await Post.findByPk(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    // Delete the post
+    await post.destroy();
+
+    res.json({ message: 'Post deleted successfully' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
 // ----------------------------------------------------------------
 // Comments Table Endpoints:
 
@@ -75,6 +139,78 @@ router.get('/comments/:id', async (req, res) => {
   }
 });
 
+router.post('/comments', async (req, res) => {
+  try {
+    const { author, content, postId } = req.body; // Get comment data from the request body
+
+    // Create the comment
+    const comment = await Comment.create({
+      author: author,
+      content: content,
+      postId: postId
+    });
+
+    res.status(201).json(comment); // Return the created comment in the response
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.patch('/comments/:id', async (req, res) => {
+  try {
+    const commentId = req.params.id; // Get the comment ID from the request parameters
+    const { author, content, postId } = req.body; // Get updated comment data from the request body
+
+    // Find the comment by its ID
+    const comment = await Comment.findByPk(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    // Update the comment with new data
+    if (author) {
+      comment.author = author;
+    }
+    if (content) {
+      comment.content = content;
+    }
+    if (postId) {
+      comment.postId = postId;
+    }
+    await comment.save();
+
+    res.json(comment); // Return the updated comment in the response
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+router.delete('/comments/:id', async (req, res) => {
+  try {
+    const commentId = req.params.id; // Get the comment ID from the request parameters
+
+    // Find the comment by its ID
+    const comment = await Comment.findByPk(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    // Delete the comment
+    await comment.destroy();
+
+    res.json({ message: 'Comment deleted successfully' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// ----------------------------------------------------------------
 
 // Export the router
 module.exports = router;
